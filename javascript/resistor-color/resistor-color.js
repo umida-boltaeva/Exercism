e297f9251e5d55809c256cac7343120acce3eda5
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2021-05-24 22:36:32 +1000

    initial commit

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+  return COLORS.findIndex((elem) => color === elem);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
