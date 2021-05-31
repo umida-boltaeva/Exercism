cd6031da0393cb95155c4a7c2a98f31cea3b9343
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2021-05-31 18:28:04 +1000

    add a constant

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -4,5 +4,6 @@
 //
 
 export const gigasecond = (moment) => {
-  return new Date(moment.getTime() + 10 ** 12);
+  const GIGASECOND_IN_MS = 10 ** 12;
+  return new Date(moment.getTime() + GIGASECOND_IN_MS);
 };
